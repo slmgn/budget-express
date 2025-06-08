20c5428679566dc2c45a5b49436327dc17be3d05
Salome Gene <salomegene@Salomes-Mac-mini.local>
2025-06-08 16:18:53 -0500

    new branch

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 print("hello World!")
+
+# 1. What is an expression?
+# An expression is a combination of values, variables, operators, and function calls that evaluates to a value.
+# A piece of code that produces a value
+
+# 2. What is a syntax error?
+# A syntax error is a mistake in the code that violates the rules of the programming language, preventing it from being parsed correctly.
+
+# 3. What does a linter do?
+# A linter analyzes code for potential errors, stylistic issues, and adherence to coding standards, providing feedback to improve code quality.
+
+students_count = 1000
+rating = 4.99
+is_publish = True
+course_name = "Python Programming"
+print(students_count)
+message = """
+Hi Jhon,
+Welcome to the Python Programming course."""
